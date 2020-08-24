92285c2d56ad9e79535b2fca56b170bc04bfc0b0
txroot <andretxrocha@tutamail.com>
2020-08-24 13:08:40 +0100

    tests about img to data

--- vendor/pdftex/pdftex.js
+++ vendor/pdftex/pdftex.js
@@ -1,6 +1,6 @@
 var PDFTeX = function(opt_workerPath) {
   if (!opt_workerPath) {
-    opt_workerPath = '/vendor/pdftex/pdftex-worker.js';
+    opt_workerPath = 'vendor/pdftex/pdftex-worker.js';
   }
   var worker = new Worker(opt_workerPath);
   var self = this;
